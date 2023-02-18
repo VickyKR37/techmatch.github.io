9adf201e9e9b880963babba4fa49141085024c33
Vicky <vickyks@hotmail.com>
2023-02-18 11:24:20 +0000

    added functions to submit login form

--- docs/main.js
+++ docs/main.js
@@ -1,3 +1,11 @@
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement,classList.add(`form__message--${type}`);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createnewccount");
@@ -12,5 +20,13 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();
         loginForm.classList.remove("form--hidden");
         createAccountForm.classList.add("form--hidden")
+    });
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        //Perform your ajax/fetch login
+
+        setFormMessage(loginForm, "error", "Invalid username/password combination");
     });
 });
