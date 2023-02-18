578d940397b9c868663d6a21a7a4cef21185c758
Vicky <vickyks@hotmail.com>
2023-02-18 10:44:33 +0000

    added html to createnewaccount.html

--- docs/main.js
+++ docs/main.js
@@ -2,13 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
     const createAccountForm = document.querySelector("#createnewccount");
 
-    document.querySelector("#linkCreateAccount").addEventListener("click", () => {
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
         loginForm.classList.add("form--hidden");
         createAccountForm.classList.remove("form--hidden")
     });
 
-    document.querySelector("#linkLogin").addEventListener("click", () => {
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
         loginForm.classList.remove("form--hidden");
         createAccountForm.classList.add("form--hidden")
-    })
+    });
 });
